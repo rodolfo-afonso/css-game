d3f1c28a2b2625427004bbec296af0a8bdf4e7af
Rodolfo Afonso <rodolfo.borges.afonso@gmail.com>
2019-12-04 21:20:40 -0300

    first commit

---
+++ bk/config.rb
@@ -0,0 +1,11 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# sass input dir
+sass_dir = ""
+
+# Output dir
+css_dir = ""
